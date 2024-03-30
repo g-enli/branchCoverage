5bbcc1713dc9c1d80de59157444ab790a1916efa
g-enli <gokcenenli@gmail.com>
2024-03-31 00:08:32 +0300

    2 tests for counter

--- src/test/java/TestCompute.java
+++ src/test/java/TestCompute.java
@@ -33,4 +33,30 @@ public class TestCompute {
     assertEquals(0,c.countNumberOfOccurrences(e));//0 bekliyorum 0 geldi olmalı
   }
   
+  @Test
+  public void testCounterEquals() {
+    MessageQueue mq = mock(MessageQueue.class);
+    c = new Compute(mq);
+    when(mq.size()).thenReturn(5);//size'a takılmasın
+    String e = anyString();
+    when(mq.contains(e)).thenReturn(true);//containse takılmasın
+    when(mq.getAt(anyInt())).thenReturn(e);
+    assertEquals(5,c.countNumberOfOccurrences(e));//count değerini dönsün
+  }
+  
+  @Test
+  public void testCounterNotEquals() {
+    MessageQueue mq = mock(MessageQueue.class);
+    c = new Compute(mq);
+    when(mq.size()).thenReturn(5);//size'a takılmasın
+    String e = anyString();
+    when(mq.contains(e)).thenReturn(true);//containse takılmasın
+    when(mq.getAt(0)).thenReturn(e);
+    when(mq.getAt(1)).thenReturn(null);
+    when(mq.getAt(2)).thenReturn(null);
+    when(mq.getAt(3)).thenReturn(null);
+    when(mq.getAt(4)).thenReturn(null);
+    assertEquals(1,c.countNumberOfOccurrences(e));//count değerini dönsün
+  }
 }
+
